787475367eccc4d1370727b29d64a2fc4ea91933
stobertonf <stobertonf@gmail.com>
2023-02-04 22:08:59 -0300

    feat(Post-Page): Creating Post Page file.

---
+++ lib/features/create_post/create_post_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreatePostPage extends StatefulWidget {
+  const CreatePostPage({Key? key}) : super(key: key);
+
+  @override
+  State<CreatePostPage> createState() => _CreatePostPageState();
+}
+
+class _CreatePostPageState extends State<CreatePostPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
