cdff1649bd869b8b545d69f466878c3ac6be5ec6
stobertonf <stobertonf@gmail.com>
2023-02-04 22:23:32 -0300

    feat(current-user): Creating Info Tab file.

---
+++ lib/features/current_user_profile/tabs/info_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InfoTab extends StatelessWidget {
+  const InfoTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
